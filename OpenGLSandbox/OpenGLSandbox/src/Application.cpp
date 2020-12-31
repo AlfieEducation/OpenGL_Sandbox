25c09a8718d6db46b4ca97a0443b0a90c90a7d38
alfie.education <alfie.education@gmail.com>
2020-12-31 15:13:16 +0300

    [OpenGL][Sandbox] Abstract error handling, index and vertex buffers to classes

--- OpenGLSandbox/OpenGLSandbox/src/Application.cpp
+++ OpenGLSandbox/OpenGLSandbox/src/Application.cpp
@@ -6,28 +6,9 @@
 #include <string>
 #include <sstream>
 
-#define ASSERT(x) if (!(x)) __debugbreak();
-
-#ifdef _DEBUG
-#define GLCall(x) GLClearError(); x; ASSERT(GLLogCall(#x, __FILE__, __LINE__))
-#else
-#define GLCall(x) x
-#endif
-
-static void GLClearError() 
-{
-	while (glGetError() != GL_NO_ERROR);
-}
-
-static bool GLLogCall(const char* function, const char* file, int line)
-{
-	while (GLenum error = glGetError()) {
-		std::cout << "[OpenGL Error] (" << error << "):" << function <<
-			" " << file << ":" << line << std::endl;
-		return false; 
-	}
-	return true;
-}
+#include "Renderer.h"
+#include "IndexBuffer.h"
+#include "VertexBuffer.h"
 
 struct ShaderProgramSource
 {
@@ -140,97 +121,88 @@ int main(void)
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	float positions [] = {
-		-0.5f, -0.5f, //0
-		 0.5f, -0.5f, //1
-		 0.5f,  0.5f, //2
-		-0.5f,  0.5f, //3
-	};
-
-	// Indices elements must be unsigned
-	unsigned int indices[] = {
-		0, 1, 2,
-		2, 3, 0
-	};
-
-	unsigned int vao;
-	GLCall(glGenVertexArrays(1, &vao));
-	GLCall(glBindVertexArray(vao));
-
-	/*Variable which contains buffer id. 
-	Everything which we generate by OpenGL shoud have id (because of it state machine) */
-	unsigned int buffer; 
-
-	/* Generate buffer*/
-	GLCall(glGenBuffers(1, &buffer));
-	GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
-	GLCall(glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW));
-
-	/* We say to OpenGL what exactly It should draw from our data*/
-	GLCall(glEnableVertexAttribArray(0));
-	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
-
-	unsigned int ibo;
-
-	/* Generate buffer*/
-	GLCall(glGenBuffers(1, &ibo));
-	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
-	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW));
-
-	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
-
-	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-	//Bind Sharer:
-	GLCall(glUseProgram(shader));
-
-	GLCall(int location = glGetUniformLocation(shader, "u_Color"));
-	ASSERT(location != -1);
-	GLCall(glUniform4f(location, 0.2f, 0.8f, 0.3f, 1.0f));
-
-	GLCall(glBindVertexArray(0));
-	GLCall(glUseProgram(0));
-	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
-	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
-
-	float r = 0.0f;
-	float increment = 0.05f;
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
 	{
-		/* Render here */
-		GLCall(glClear(GL_COLOR_BUFFER_BIT));
+		float positions[] = {
+			-0.5f, -0.5f, //0
+			 0.5f, -0.5f, //1
+			 0.5f,  0.5f, //2
+			-0.5f,  0.5f, //3
+		};
 
-		GLCall(glUseProgram(shader));
-		GLCall(glUniform4f(location, r, 0.8f, 0.3f, 1.0f));
+		// Indices elements must be unsigned
+		unsigned int indices[] = {
+			0, 1, 2,
+			2, 3, 0
+		};
 
+		unsigned int vao;
+		GLCall(glGenVertexArrays(1, &vao));
 		GLCall(glBindVertexArray(vao));
-		//GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
-		//GLCall(glEnableVertexAttribArray(0));
-		//GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
 
-		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
 
-		if (r > 1.0f)
-			increment = -0.05f;
-		else if (r < 0.0f)
-			increment = 0.05f;
+		VertexBuffer vb(positions, sizeof(positions));
 
-		r += increment;
+		/* We say to OpenGL what exactly It should draw from our data*/
+		GLCall(glEnableVertexAttribArray(0));
+		GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
 
-		/*  Render primitives from array data
-		first parameter - Specifies what kind of primitives to render.
-		second parameter - Specifies the type of the values in indices. Must be one of GL_UNSIGNED_BYTE, GL_UNSIGNED_SHORT, or GL_UNSIGNED_INT.
-		third parameter - Specifies an offset of the first index in the array in the data store of the buffer currently bound to the GL_ELEMENT_ARRAY_BUFFER target.*/
-		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr)); //6 - number of indices. Must be UNSIGNED INT
+		IndexBuffer ib(indices, 6);
 
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
+		ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
 
-		/* Poll for and process events */
-		glfwPollEvents();
+		unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+		//Bind Sharer:
+		GLCall(glUseProgram(shader));
+
+		GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+		ASSERT(location != -1);
+		GLCall(glUniform4f(location, 0.2f, 0.8f, 0.3f, 1.0f));
+
+		GLCall(glBindVertexArray(0));
+		GLCall(glUseProgram(0));
+		GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
+		float r = 0.0f;
+		float increment = 0.05f;
+		/* Loop until the user closes the window */
+		while (!glfwWindowShouldClose(window))
+		{
+			/* Render here */
+			GLCall(glClear(GL_COLOR_BUFFER_BIT));
+
+			GLCall(glUseProgram(shader));
+			GLCall(glUniform4f(location, r, 0.8f, 0.3f, 1.0f));
+
+			GLCall(glBindVertexArray(vao));
+			//GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+			//GLCall(glEnableVertexAttribArray(0));
+			//GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
+
+			ib.Bind();
+
+			if (r > 1.0f)
+				increment = -0.05f;
+			else if (r < 0.0f)
+				increment = 0.05f;
+
+			r += increment;
+
+			/*  Render primitives from array data
+			first parameter - Specifies what kind of primitives to render.
+			second parameter - Specifies the type of the values in indices. Must be one of GL_UNSIGNED_BYTE, GL_UNSIGNED_SHORT, or GL_UNSIGNED_INT.
+			third parameter - Specifies an offset of the first index in the array in the data store of the buffer currently bound to the GL_ELEMENT_ARRAY_BUFFER target.*/
+			GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr)); //6 - number of indices. Must be UNSIGNED INT
+
+			/* Swap front and back buffers */
+			glfwSwapBuffers(window);
+
+			/* Poll for and process events */
+			glfwPollEvents();
+		}
+
+		GLCall(glDeleteProgram(shader));
 	}
-
-	GLCall(glDeleteProgram(shader));
 
 	glfwTerminate();
 	return 0;
